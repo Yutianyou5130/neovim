a66e80d9524cdd3b2a77b5639d6c1bdbaeca6641
nkym <2738684393@qq.com>
2023-06-11 15:02:04 +0800

    problely final commit

--- init.lua
+++ init.lua
@@ -103,7 +103,15 @@ require("lazy").setup({
 		"williamboman/mason.nvim",
 		build = ":MasonUpdate",
 		config = function()
-			require("mason").setup()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
 		end,
 	},
 	{
